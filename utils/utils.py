204c539cc2a3159816abd56b7e930df168517053
JeasunLok <luojsh7@mail2.sysu.edu.cn>
2023-08-16 17:31:33 +0800

    add: framework commit

---
+++ utils/utils.py
@@ -0,0 +1,58 @@
+import numpy as np
+from PIL import Image
+# a class for calculating the average of the accuracy and the loss
+#-------------------------------------------------------------------------------
+class AverageMeter(object):
+
+  def __init__(self):
+    self.reset()
+
+  def reset(self):
+    self.average = 0 
+    self.sum = 0
+    self.count = 0
+
+  def update(self, val, n=1):
+    self.sum += val * n
+    self.count += n
+    self.average = self.sum / self.count
+#-------------------------------------------------------------------------------
+
+def show_config(**kwargs):
+    print('Configurations:')
+    print('-' * 70)
+    print('|%25s | %40s|' % ('keys', 'values'))
+    print('-' * 70)
+    for key, value in kwargs.items():
+        print('|%25s | %40s|' % (str(key), str(value)))
+    print('-' * 70)
+
+def get_lr(optimizer):
+    for param_group in optimizer.param_groups:
+        return param_group['lr']
+    
+def cvtColor(image):
+    if len(np.shape(image)) == 3 and np.shape(image)[2] == 3:
+        return image 
+    else:
+        image = image.convert('RGB')
+        return image 
+
+# 对输入图像进行resize
+def resize_image(image, size):
+    iw, ih  = image.size
+    w, h    = size
+
+    scale   = min(w/iw, h/ih)
+    nw      = int(iw*scale)
+    nh      = int(ih*scale)
+
+    image   = image.resize((nw,nh), Image.BICUBIC)
+    new_image = Image.new('RGB', size, (128,128,128))
+    new_image.paste(image, ((w-nw)//2, (h-nh)//2))
+
+    return new_image, nw, nh
+
+def preprocess_input(image):
+    image /= 255.0
+    return image
