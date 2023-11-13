c12e825660afdfb9c29a60a1787dfead1dd6233b
JeasunLok <1913587141@qq.com>
2023-11-13 18:27:55 +0800

    add: UNet model

---
+++ model/UNet.py
@@ -0,0 +1,109 @@
+import torch.nn as nn
+import torch
+import torch.nn.functional as F
+from torchsummary import summary
+from thop import profile
+
+class DoubleConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.double_conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+    def forward(self, x):
+        return self.double_conv(x)
+    
+class Down(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.maxpool_conv = nn.Sequential(
+            nn.MaxPool2d(2),
+            DoubleConv(in_channels, out_channels)
+        )
+ 
+    def forward(self, x):
+        return self.maxpool_conv(x)
+
+class Up(nn.Module):
+    def __init__(self, in_channels, out_channels, bilinear=True):
+        super().__init__()
+        if bilinear:
+            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+        else:
+            self.up = nn.ConvTranspose2d(in_channels // 2, in_channels // 2, kernel_size=2, stride=2)# //为整数除法
+ 
+        self.conv = DoubleConv(in_channels, out_channels)
+ 
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+        diffY = torch.tensor([x2.size()[2] - x1.size()[2]])
+        diffX = torch.tensor([x2.size()[3] - x1.size()[3]])
+ 
+        x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
+                        diffY // 2, diffY - diffY // 2])
+ 
+        x = torch.cat([x2, x1], dim=1)
+        return self.conv(x)
+
+class OutConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(OutConv, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
+ 
+    def forward(self, x):
+        return self.conv(x)
+    
+class UNet(nn.Module):
+    def __init__(self, num_classes, num_channels=3, bilinear=True):
+        super(UNet, self).__init__()
+        self.n_channels = num_channels # 输入通道数
+        self.n_classes = num_classes # 输出类别数
+        self.bilinear = bilinear # 上采样方式
+ 
+        self.inc = DoubleConv(num_channels, 64) # 输入层
+        self.down1 = Down(64, 128)
+        self.down2 = Down(128, 256)
+        self.down3 = Down(256, 512)
+        self.down4 = Down(512, 512)
+        self.up1 = Up(1024, 256, bilinear)
+        self.up2 = Up(512, 128, bilinear)
+        self.up3 = Up(256, 64, bilinear)
+        self.up4 = Up(128, 64, bilinear)
+        self.outc = OutConv(64, num_classes) # 输出层
+ 
+    def forward(self, x):
+        x1 = self.inc(x) # 一开始输入
+        x2 = self.down1(x1) # 四层左部分
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x = self.up1(x5, x4) # 四层右部分
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        logits = self.outc(x) # 最终输出
+        return logits
+
+if __name__ == '__main__':
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = UNet(num_classes=2).to(device)
+    input = torch.randn([2,3,512,512]).to(device)
+    total_params = sum(p.numel() for p in model.parameters())
+    print("="*64)
+    print(f'{total_params:,} total parameters.')
+    total_trainable_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    print(f'{total_trainable_params:,} training parameters.')
+    print(model(input).shape)
+    flops, params = profile(model, inputs=(input,))
+    print("="*64)
+    print("flops:{:.3f}M".format(flops / 1e6))
+    print("params:{:.3f}M".format(params / 1e6))
+    # --------------------------------------------------#
+    #   用来测试网络能否跑通，同时可查看FLOPs和params
+    # --------------------------------------------------#
+    summary(model, input_size=(3,512,512), batch_size=-1)
